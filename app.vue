572305aaacec4400624bd4c817d9fdd9677cccec
luth <luthalto@gmail.com>
2023-03-10 00:25:10 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <v-container>
+      <div
+        v-ripple
+        class="text-center elevation-2 pa-12 text-h5"
+      >
+        HTML element with v-ripple
+      </div>
+    </v-container>
+  </div>
+</template>
---
+++ src/components/Pages/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <NuxtLink href="https://google.com">
+    <v-toolbar-title> amplify-nuxt3-sample </v-toolbar-title>
+  </NuxtLink>
+</template>
+<!-- <script setup lang="ts">
+
+</script> -->
