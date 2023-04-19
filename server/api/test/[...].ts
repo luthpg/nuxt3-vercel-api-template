3f6ab055180c9d594e6b3885c7fdbfd59d9c05d0
luth <luthalto@gmail.com>
2023-04-19 22:11:28 +0900

    server base

---
+++ server/api/test/[...].ts
@@ -0,0 +1,1 @@
+export default defineEventHandler(() => `Default test handler`);
---
+++ server/api/validation/[id].ts
@@ -0,0 +1,10 @@
+export default defineEventHandler((event) => {
+  const id = parseInt(event.context.params?.id as string) as number;
+  if (!Number.isInteger(id)) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "ID should be an integer",
+    });
+  }
+  return "All good";
+});
