404a1c6a90b44bff07e5a05a19dcd25ffdf2687f
luth <88308177+luthpg@users.noreply.github.com>
2023-10-12 13:11:59 +0000

    add basic-auth module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from "nuxt/config";
 import vuetify from "vite-plugin-vuetify";
+import BasicAuth from 'nuxt-basic-authentication-module';
 
 export default defineNuxtConfig({
   build: {
     transpile: ["vuetify"],
   },
+  modules: [
+    [BasicAuth, { enabled: true }]
+  ],
   hooks: {
     "vite:extendConfig": (config) => {
       config.plugins!.push(vuetify());
@@ -20,4 +24,14 @@ export default defineNuxtConfig({
     },
   },
   css: ["@/assets/main.scss"],
+  runtimeConfig: {
+    basicAuth: {
+      productionDomains: [
+        // 'example-stage.vercel.app', // 本番環境ドメイン endWithマッチ
+      ],
+      pairs: {
+        // admin: process.env.BASIC_ADMIN_PIN as string, // Passwords
+      }
+    }
+  }
 });
